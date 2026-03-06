0422fbbecf1247fc42bbb1eeb7f94e45e6ca2a6a
PMT201 <PMT201@PMT3012>
2026-03-06 09:12:46 +0700

    jn,j

--- KyThuatLapTrinh/buoi1/buoi1.cpp
+++ KyThuatLapTrinh/buoi1/buoi1.cpp
@@ -1,6 +1,13 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 
 int main() {
+	vector <float> diemthi;
+	diemthi.push_back(5.5);
+	diemthi.push_back(4);
+	diemthi.push_back(6);
+	diemthi.pop_back();
+
 
 }
